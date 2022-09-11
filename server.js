80e174f5062b5f5cf3eb13129df8ae2780ffaeeb
git geek <cout.1.endl@gmail.com>
2022-09-10 22:37:24 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,6 @@
+const employee = require("./lib/Employee");
+const engineer = require("./lib/Engineer");
+const intern = require("./lib/Intern");
+const manager = require("./lib/Manager");
+
+const fs = require("fs");
